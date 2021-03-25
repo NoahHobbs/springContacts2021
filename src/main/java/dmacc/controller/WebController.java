efaa392be05fa544a2d3465776627785c8be4229
Noah Hobbs <mlzgphoenix@gmail.com>
2021-03-25 14:02:06 -0500

    Made some changes

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import dmacc.beans.Contact;
 import dmacc.repository.ContactRepository;
 
 @Controller
@@ -16,8 +19,22 @@ public class WebController {
 	@GetMapping("/viewAll")
 	public String viewAllContacts(Model model) {
 		model.addAttribute("contacts", repo.findAll());
-		return "results";
+		return "results ";
 	}
+	
+	@GetMapping("/inputContact")
+	public String addNewContact(Model model) {
+		Contact c = new Contact();
+		model.addAttribute("newContact", c);
+		return "input";
+	}
+	
+	@PostMapping("/inputContact")
+	public String addNewContact(@ModelAttribute Contact c, Model model) {
+		repo.save(c);
+		return viewAllContacts(model);
+	}
+	
 }
 
 
